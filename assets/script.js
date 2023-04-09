3e80a1105f976cc96a757e01cd8fa47636c88b7a
Tamara Sferco <122523521+TamaraDawg@users.noreply.github.com>
2023-04-09 13:32:37 +1000

    Successfully reads input city

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,37 @@
 //api related
 const apiKey = '439e2addbc18b8368ececfb29ed3a010';
-const lat = ;
-const lon= ;
+const searchBtn = document.getElementById('searchbtn')
+//const lat = ;
+//const lon= ;
 
 //currentdate and city section
+var currentDate = dayjs().format("MMM D, YYYY");
+document.getElementById("currentDate").textContent = currentDate;
+
+searchBtn.addEventListener("click", searchCity);
+
+
+function searchCity(event) {
+    event.preventDefault()
+    var cityInput = document.querySelector("#city");
+    var city = cityInput.value;
+    console.log(city);
+    localStorage.setItem('city', city);
+    loadResults();
+} 
+
+
+
+function loadResults() { 
+const city = localStorage.getItem('city'); // replace with user input
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+  .then(response => response.json())
+  .then(data => {
+    // handle the weather data here
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+}
